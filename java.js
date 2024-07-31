0e339d4b93e0afd51f16785eb0a09d3d88b69600
Luiz-Felipe-Dorneles <159260456+Luiz-Felipe-Dorneles@users.noreply.github.com>
2024-07-31 10:20:23 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,20 @@
+
+function conteudo1() {
+    document.getElementById("conteudo").innerHTML = "<p>Foto Aérea de Nazaré</p>"
+    document.getElementById("imagem").innerHTML = "<img src='Foto1.jpg'>"
+}
+function conteudo2() {
+    document.getElementById("conteudo").innerHTML = "<p>Farol de São Miguel Arcanjo. <br> É o principal ponto onde acontece as Maiores ondas do mundo.</p>"
+    document.getElementById("imagem").innerHTML = "<img src='Foto2.jpg'>"
+}
+
+function conteudo3() {
+    document.getElementById("conteudo").innerHTML = "<p>Cidade Principal de Nazaré</p>"
+    document.getElementById("imagem").innerHTML = "<img src='Foto3.jpg'>"
+}
+
+function conteudo4() {
+    document.getElementById("conteudo").innerHTML = "<p>Maior onda já vista em Nazaré. Somente três pessoas conquistaram <br> o recorde de maior onda já surfada em Nazaré: o pioneiro Garrett McNamara, <br> o brasileiro Rodrigo Coxa (conhecido como CoxaBomb) e o alemão Sebastian Steudtner.</p>"
+    document.getElementById("imagem").innerHTML = "<img src='Foto4.jpg'>"
+}
+
